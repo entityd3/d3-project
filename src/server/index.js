5d425d92943cd7bb2f4ac22bd4669ea688babc80
EntityD3 <entityd3@users.noreply.github.com>
2015-11-24 18:41:52 -0600

    Math questions: + works

--- src/server/index.js
+++ src/server/index.js
@@ -1,34 +1,50 @@
-import prompt from 'prompt';
+import readline from 'readline';
+import Chance from 'chance';
 
-function askTheQuestion() {
-  console.log(`
- 1. quack
- 2. bark
- 3. meow
- 3.14159265.PIE!
-`);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-  prompt.get('option', (err, res) => {
-    switch (res.option) {
-      case '1':
-        console.log('quack');
+const chance = new Chance();
+
+function go() {
+  rl.question('What sort of math question do you want? (+, -, *, /) ', op => {
+    console.log(`You picked ${op}`);
+
+    const arg1 = chance.integer({min: 0, max: 100});
+    const arg2 = chance.integer({min: 0, max: 100});
+
+    rl.question(`${arg1} ${op} ${arg2} = `, answer => {
+      console.log(`You said ${answer}`);
+
+      // answer is a string, but to compare it, we need it to be an int
+      // parseInt() converts a string to an int.
+      answer = parseInt(answer);
+
+      switch (op) {
+      case '+':
+        const expected = arg1 + arg2;
+
+        if (answer === expected) {
+          console.log('Good Job =)');
+        } else {
+          console.log(`Nice Try. It should have been ${expected}`);
+        }
         break;
-      case '2':
-        console.log('bark');
+      case '-':
         break;
-      case '3':
-        console.log('meow');
+      case '*':
+        break;
+      case '/':
         break;
       default:
-        console.log("I don't know how to do that");
         break;
-      case '3.14159265':
-        console.log('PIE!');
-        break;
-    }
+      }
 
-    askTheQuestion();
+      go();
+    });
   });
 }
 
-askTheQuestion();
+go();
